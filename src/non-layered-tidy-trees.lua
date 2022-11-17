08f6fcb10702052cad0651a0176b5704db8f3ca9
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-11-17 17:33:39 +0100

    Inverse relation

--- src/non-layered-tidy-trees.lua
+++ src/non-layered-tidy-trees.lua
@@ -9,9 +9,22 @@ function nonlayeredtidytrees.trees (rel)
 
     local tree = {}
 
-    for k, each in pairs (rel) do tree[k] = libluanonlayeredtidytrees.mktree (k, each.w, each.h, each.y or 0.0, #each.c) end
+    local newrel = {}
 
-    for k, parent in pairs (rel) do
+    for k, each in pairs (rel) do
+
+        local newc = {}
+        for i, c in ipairs (each.c) do newc[i] = -c end
+        local neweach = {w = each.w, h = each.h, y = each.y, c = newc }
+        newrel[k] = neweach
+        newrel[-k] = { w = 0, h = 1, y = 0, c = { k } }
+    end
+
+    for k, each in pairs (newrel) do 
+        tree[k] = libluanonlayeredtidytrees.mktree (k, each.w, each.h, each.y or 0.0, #each.c)
+    end
+
+    for k, parent in pairs (newrel) do
 
         for i, child in ipairs (parent.c) do     -- really important to keep the order of children.
 
--- test/test.lua
+++ test/test.lua
@@ -2,46 +2,6 @@
 local lu = require 'luaunit'
 local tidy = require 'non-layered-tidy-trees'
 
-function est_simple_input ()
-
-    local tree = tidy.trees {
-
-        [0] = {w = 70, h = 30, c = {1, 2, 3, 7}},
-        [1] = {w = 10, h = 50, c = {}},
-        [2] = {w = 20, h = 30, c = {}},
-        [3] = {w = 30, h = 30, c = {4, 6}},
-        [4] = {w = 20, h = 50, c = {5}},
-        [5] = {w = 40, h = 10, c = {}},
-        [6] = {w = 20, h = 20, c = {}},
-        [7] = {w = 30, h = 50, c = {8, 9}},
-        [8] = {w = 30, h = 20, c = {}},
-        [9] = {w = 10, h = 10, c = {}},
-
-    }
-
-    local root = tree[0]
-
-    --tidy.layout (root)
-
-    local result = {}
-
-    for k, t in pairs (tree) do
-
-        local tbl = tidy.dbind (t)
-
-        lu.assertEquals (k, tbl.id)
-        result[k] = tbl
-
-    end
-
-    tidy.free(root);
-
-    lu.assertEquals (result, {})
-
-
-
-end
-
 function test_simple ()
 
     local tree = tidy.trees {
@@ -58,8 +18,8 @@ function test_simple ()
         [10] = {w = 10, h = 10, c = {}},
 
     }
-
-    local root = tree[1]
+    
+    local root = tree[-1]
 
     tidy.layout (root)
 
